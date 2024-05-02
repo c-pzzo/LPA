a5db7bd2ae282c4f0db342ce9f3bf6e745cc15c6
c_pzzo <cesar.pozo@infosapiens.mx>
2024-05-01 20:55:50 -0600

    Minor nomenclature adjustments

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Assuming images are stored in the 'static/images' folder
     image_folder = 'static/images'
     carousel_images = sorted(os.listdir(image_folder))
     return render_template('main.html', carousel_images=carousel_images)
