cb013ab3fb041b8bf22706308a1526505dc515f0
c_pzzo <cesar.pozo@infosapiens.mx>
2024-05-01 20:40:04 -0600

    Added dynamic image display with Flask

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def greeting():
-    return render_template("main.html")
+@app.route('/')
+def index():
+    # Assuming images are stored in the 'static/images' folder
+    image_folder = 'static/images'
+    carousel_images = sorted(os.listdir(image_folder))
+    return render_template('main.html', carousel_images=carousel_images)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
